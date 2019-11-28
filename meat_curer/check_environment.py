dac21972504fb7d91e434047512520502f8bd3fb
Dane Morgan <32076156+git-gud-1@users.noreply.github.com>
2019-11-28 11:11:35 -0700

    Update check_environment.py

--- meat_curer/check_environment.py
+++ meat_curer/check_environment.py
@@ -6,24 +6,6 @@ from Devices import Relay, HTSensor, PinAssignments
 from Meat import Meat
 
 
-def lock(device) -> None:
-    return
-
-
-def release_lock(device) -> None:
-    return
-
-
-def setup_devices() -> dict:
-    ht_pin_1= PinAssignments.get('HT1')
-    relay_pin_1 = PinAssignments.get('RELAY1')
-
-    HT = HTSensor(ht_pin_1)
-    R1 = Relay(relay_pin_1)
-
-    return {'HT1': HT, 'RELAY1': R1}
-
-
 def check_temp(temp, meat) -> list:
     if temp > meat.t_high:
         return ['COMP_ON', 'LAMP_OFF']
